9ad391e9d16e1fa693f97f6ab80b0f77ed2b884c
nining <nining@gstianfu.com>
2018-05-14 18:31:05 +0800

    add reduce

--- 23UserfullModules/functools_.py
+++ 23UserfullModules/functools_.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*-
+from functools import reduce
 
-import functools
+filter_con_list = list()
+
+filter_con_list.append(True)
+filter_con_list.append(True)
+filter_con_list.append(True)
+
+res = reduce(lambda x, y: (x & y), filter_con_list)  # 先取前两个元素调用函数，得到结果再和下一个元素调用函数
+
+print(res)
